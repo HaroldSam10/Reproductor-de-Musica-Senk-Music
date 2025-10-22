1ba0cea947b01878881d595ff3615a0918b2f309
harold <yamada.yeshua@gmail.com>
2025-10-21 23:14:49 -0600

    Login Finalizado

--- app/src/main/java/com/example/senkmusic/Login.kt
+++ app/src/main/java/com/example/senkmusic/Login.kt
@@ -1,15 +1,78 @@
 package com.example.senkmusic
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.os.Handler ///LIBRERIA PARA EL INICIO DE FOTOS DINAMICO
+import android.os.Looper  ///LIBRERIA PARA EL INICIO DE FOTOS DINAMICO
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide() // <-- AÑADE ESTA LÍNEA AQUÍ
+
+        // Iniciar la animación de cambio de fondo
+        iniciarCambioFondo()
+
+        //NUESTROS NODOS DE LOS ELEMETOS DEL LOGIN
+        val edtUser = findViewById<EditText>(R.id.edtUser)
+        val edtPass = findViewById<EditText>(R.id.edtPass)
+        val btn_Inicio = findViewById<Button>(R.id.button)
+
+        val enid = "harold1417"
+        val enid2 = "renosenk1417"
+
+        btn_Inicio.setOnClickListener{
+            val user_input = edtUser.text.toString()
+            val pass_input = edtPass.text.toString()
+
+            if (user_input == enid && pass_input == enid2) {
+                Toast.makeText(this, "TUS CREDENCIALES SON CORRECTAS", Toast.LENGTH_LONG).show()
+
+
+                val intent = Intent(this@Login, index_music::class.java)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(this, "TUS CREDENCIALES SON INCORRECTAS", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private val fondos = arrayOf(
         R.drawable.car_login_1,
@@ -18,18 +81,6 @@ class Login : AppCompatActivity() {
     )
 
     private var index = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-
-
-
-        // Iniciar la animación de cambio de fondo
-        iniciarCambioFondo()
-    }
-
 
     ///FONDOS DINAMICOSSS
     private fun cambiarFondo() {
